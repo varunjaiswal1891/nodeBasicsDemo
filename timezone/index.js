f8bcfc65845e4e98c37f1423258722d39b94415c
Varun Jaiswal <varunpandey511@gmail.com>
2024-03-04 18:31:01 +0530

    test timezone comment

--- timezone/index.js
+++ timezone/index.js
@@ -2,7 +2,7 @@
 //npm install moment - library to work with timezones
 
 //const lodash = require('lodash'); //lodash is a utility library that provides a lot of useful methods for working with arrays, collections, and objects
-
+//test message
 //console.log(moment().format('dddd'));
 //console.log(lodash.random(1, 10)); //random number between 1 and 10
 
